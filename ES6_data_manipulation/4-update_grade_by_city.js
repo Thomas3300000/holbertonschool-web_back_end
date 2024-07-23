48f54a8da35605633de4e9ad45b47a1486274f1b
Thomas3300000 <8844@holbertonstudents.com>
2024-07-23 18:09:52 +0200

    Update grade students filter by city

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+export default function updateStudentGradeByCity(listStu, city, newGrades) {
+    const cityList = listStu.filter((student) => student.location === city);
+ 
+    const upStudents = cityList.map((student) => {
+      const gradeStudent = newGrades.find((grade) => grade.studentId === student.id);
+      const grade = gradeStudent ? gradeStudent.grade : 'N/A';
+      return { ...student, grade: grade };
+    });
+ 
+    return upStudents;
+  }
