2c8583f6e51201d10cfe7a8115d62b63163dba73
Thomas3300000 <8844@holbertonstudents.com>
2024-07-24 11:56:17 +0200

    Fix indentation

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -6,4 +6,4 @@ export default function cleanSet(set, startString) {
     .filter((value) => value.startsWith(startString))
     .map((value) => value.slice(startString.length))
     .join('-');
-  }
+}
