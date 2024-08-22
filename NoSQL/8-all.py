a8fc3a2040c1042aa35125432504dccfd2fa1876
Thomas3300000 <8844@holbertonstudents.com>
2024-08-22 21:45:21 +0200

    Fix

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -3,7 +3,8 @@
 
 
 def list_all(mongo_collection):
+    """Lists all documents in a collection"""
     if mongo_collection is None:
         return []
-    
+
     return list(mongo_collection.find())
