182bc6d8907dd89fffdc51039709564d6d25b368
Thomas3300000 <8844@holbertonstudents.com>
2024-09-19 12:25:08 +0200

    Fix code

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -5,9 +5,6 @@ function countStudents(path) {
     const data = fs.readFileSync(path, 'utf8');
     const lines = data.split('\n');
     const cleanLines = lines.filter((line) => line.trim() !== '');
-    if (cleanLines.length <= 1) {
-      throw new Error('Aucun étudiant trouvé dans la base de données.');
-    }
 
     const students = cleanLines.slice(1);
 
@@ -31,8 +28,7 @@ function countStudents(path) {
       console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
     }
   } catch (err) {
-    throw new Error('Cannot load the database');
-  }
+    throw new Error('Cannot load the database');}
 }
 
 module.exports = countStudents;
